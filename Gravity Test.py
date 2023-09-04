7eb3d3786ff430d97755a212cb264bd8f6bce6f0
Ethan K <psyman5.ek@gmail.com>
2023-09-04 00:26:17 -0400

    Added Gravity. Added Coloring.

---
+++ Gravity Test.py
@@ -0,0 +1,159 @@
+import vpython as vp
+import numpy as np
+import random as r
+
+sphere1 = vp.sphere(pos= vp.vector(0,0,0), radius = 1, make_trail = True)
+sphere1.velocity = vp.vector(1,5,4)
+sphere1.mass = 5
+sphere1.p = vp.vector(sphere1.velocity.x * sphere1.mass, sphere1.velocity.y * sphere1.mass,sphere1.velocity.z * sphere1.mass)
+
+#box = vp.box(pos = vp.vector(0,0,0), size = vp.vector(20,20,20))
+
+
+boundingShape = False
+
+boundingField = (100,100,100)
+
+if boundingShape is True:
+    box = vp.box(pos = vp.vector(0,0,0), size = vp.vector(boundingField[0],boundingField[1],boundingField[2]))
+
+timeList = []
+pList = []
+
+rate = 1000
+
+debugMode = False
+
+t = 0
+dt = .001
+
+momentumGraph = vp.gcurve(color = vp.color.cyan, fast = True)
+
+g = -9.81
+
+gravity = vp.vector(0, -g ,0)
+
+def sphereMoveMethod1(sphere1, dt):
+    sphere1.pos += sphere1.velocity
+    sphere1.velocity.y += g * dt**2
+    sphere1.p.y = sphere1.velocity.y * sphere1.mass
+    return sphere1.pos, sphere1.velocity.y
+
+def sphereMoveMethod2(sphere1,dt):
+    sphere1.pos.x = sphere1.pos.x + (sphere1.velocity.x) * dt
+
+    sphere1.pos.y = sphere1.pos.y + (sphere1.velocity.y) * dt
+
+    sphere1.velocity.y += g * dt**2
+
+
+    sphere1.pos.z = sphere1.pos.z + (sphere1.velocity.z) * dt
+
+
+def sphereMoveMethod3(sphere1, dt):
+    sphere1.pos += sphere1.velocity * dt
+    sphere1.velocity.y += g * dt
+    return sphere1.pos, sphere1.velocity.y
+
+while True:
+    vp.rate(rate)
+    t = t + dt
+
+    sphere1.pos, sphere1.velocity.y = sphereMoveMethod3(sphere1, dt)[0] , sphereMoveMethod3(sphere1, dt)[1]
+
+    #sphere1.p.y = sphere1.velocity.y * sphere1.mass
+
+    if sphere1.pos.x + sphere1.radius >= boundingField[0]/2 or sphere1.pos.x - sphere1.radius <= -boundingField[1]/2:
+        if sphere1.pos.x > 0:
+            sphere1.velocity.x = -sphere1.velocity.x
+        else:
+            sphere1.velocity.x = abs(sphere1.velocity.x)
+
+    if sphere1.pos.y + sphere1.radius >= boundingField[1]/2 or sphere1.pos.y - sphere1.radius <= -boundingField[1]/2:
+        if sphere1.pos.y > 0:
+            sphere1.velocity.y = -sphere1.velocity.y
+        else:
+            sphere1.velocity.y = abs(sphere1.velocity.y)
+
+    if sphere1.pos.z + sphere1.radius >= boundingField[2]/2 or sphere1.pos.z - sphere1.radius <= -boundingField[1]/2:
+        if sphere1.pos.z > 0:
+            sphere1.velocity.z = -sphere1.velocity.z
+        else:
+            sphere1.velocity.z = abs(sphere1.velocity.z)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    '''t += dt
+    timeList.append(t)
+    vp.rate(rate)
+
+    if sphere1.pos.x + sphere1.radius >= boundingField[0]/2 or sphere1.pos.x - sphere1.radius <= -boundingField[0]/2:
+            sphere1.velocity.x = -sphere1.velocity.x
+            if debugMode == True:
+                print(f'X Collision at: {sphere1.pos.x}')
+
+    if sphere1.pos.y + sphere1.radius >= boundingField[1]/2 or sphere1.pos.y - sphere1.radius <= -boundingField[1]/2:
+        sphere1.velocity.y = -sphere1.velocity.y
+        if debugMode == True:
+                print(f'Y Collision at: {sphere1.pos.y}')
+            
+    if sphere1.pos.z + sphere1.radius >= boundingField[2]/2 or sphere1.pos.z - sphere1.radius <= -boundingField[2]/2:
+        sphere1.velocity.z = -sphere1.velocity.z
+        if debugMode == True:
+            print(f'Z Collision at: {sphere1.pos.z}')
+
+    elif abs(sphere1.p.y) != 0 or sphere1.pos.y >= boundingField[1]:
+        sphere1.pos = (sphere1.pos) + ((sphere1.p) * vp.mag(sphere1.mass)**-1) * dt
+        sphere1.velocity.y += -gravity.y
+        #if -.1 < sphere1.p.y < .1 and sphere1.pos.y - sphere1.radius <= -10:
+            #sphere1.p.y = 0
+        #else:
+        sphere1.p.y = sphere1.velocity.y * sphere1.mass.y
+        sphere1.p.x = sphere1.velocity.x * sphere1.mass.x
+        sphere1.p.z = sphere1.velocity.z* sphere1.mass.z
+        #pList.append([sphere1.p.y])
+    
+        #momentumGraph.plot(t, abs(sphere1.p.y)/(rate/10), interval = 20)'''
+    
+
+ 
--- VisualPythonApproach.py
+++ VisualPythonApproach.py
@@ -38,22 +38,28 @@ scene = vp.canvas(width = 1920, height = 1080)
 
 gravityVector = vp.vector(0, -9.81,0)
 
-particleCount = 100
+particleCount = abs(100)
 
-boundingField = (500,500,500)
+rate = 3000
 
-startPosRng = [s for s in range(-200, 200)]
+boundingField = (6000,6000,6000)
 
-veloRng = [s for s in range(-10, 10) if s != 0]
+startPosRng = [s for s in range(int(-boundingField[0]/2), int(boundingField[0]/2))]
 
-sizeRng = [s for s in range(2) if s != 0]
+veloRng = [s for s in range(-2000, 2000) if s != 0]
+
+sizeRng = [s for s in range(90) if s != 0]
 
 #particleList = [SphereParticle(position = (r.choice(rng),r.choice(rng), r.choice(rng)), velocity = (r.choice(rng),r.choice(rng), r.choice(rng)), size = r.randint(0,3), mass = 0) for s in range(0, particleCount)] 
 
-c = vp.curve()
+if particleCount < 100:
+    sphereList = [vp.sphere(pos = vp.vector(r.choice(startPosRng),r.choice(startPosRng), r.choice(startPosRng)), radius = r.choice(sizeRng), make_trail = False, emissive = False) for p in range(particleCount)]
+else:
+    sphereList = [vp.simple_sphere(pos = vp.vector(r.choice(startPosRng),r.choice(startPosRng), r.choice(startPosRng)), radius = r.choice(sizeRng), make_trail = False, emissive = False) for p in range(particleCount)]
 
-sphereList = [vp.sphere(pos = vp.vector(r.choice(startPosRng),r.choice(startPosRng), r.choice(startPosRng)), radius = r.choice(sizeRng), make_trail = False) for p in range(particleCount)]
 sphereArray = np.array(sphereList)
+
+curveList = []
 
 '''for s in sphereList:
     place = sphereList.index(s)
@@ -75,10 +81,12 @@ sphereVeloArray = np.array(sphereVelocities)
 
 #sphereVelocities = [vp.vector(r.choice(rng),r.choice(rng),r.choice(rng)) for s in sphereList]
 
-boundingShape = True
+boundingShape = False
 
-if boundingShape == True:
-    boundingBox = vp.box(pos = vp.vector(0,0,0), size  = vp.vector(boundingField[0],boundingField[1],boundingField[2]), thickness = 1)
+if boundingShape is True:
+    #boundingBox = vp.box(pos = vp.vector(0,0,0), size  = vp.vector(boundingField[0],boundingField[1],boundingField[2]), thickness = 1)
+    boundingSphere = vp.sphere(pos = vp.vector(0,0,0), radius = boundingField[0])
+
 
 
 #sphereOne = vp.sphere(pos = vp.vector(0,0,0), radius = .5)
@@ -87,73 +95,114 @@ if boundingShape == True:
 
 numberOfCalculations= []
 
+
+
 debugMode = False
 
-dt = 1/100
+dt = 1/rate
 t = 0
+
+coloringMode = True
 
 simRunning = True
 
-while simRunning == True:
-    vp.rate(100)
-    t = t + dt
+while simRunning is True:
+    try:
+        vp.rate(rate)
+        t = t + dt
 
-    for o in np.nditer(sphereArray, flags=['refs_ok','zerosize_ok']):
+        for o in np.nditer(sphereArray, flags=['refs_ok','zerosize_ok']):
 
-        s = o.item()
+            s = o.item()
 
-        otherSphereList = [e for e in sphereList if e != s and math.sqrt(((e.pos.x-s.pos.x)**2)+((e.pos.y-s.pos.y)**2)+(((e.pos.z-s.pos.z)**2))) < 10]
-        otherSphereArray = np.array(otherSphereList)
-        '''if debugMode == True:
-            numberOfCalculations.append(len(otherSphereList))'''
-        
-        
-        for u in np.nditer(otherSphereArray, flags=['refs_ok','zerosize_ok']):
+            otherSphereList = [e for e in sphereList if e != s and math.sqrt(((e.pos.x-s.pos.x)**2)+((e.pos.y-s.pos.y)**2)+(((e.pos.z-s.pos.z)**2))) < s.radius*4]
+            otherSphereArray = np.array(otherSphereList)
+            '''if debugMode == True:
+                numberOfCalculations.append(len(otherSphereList))'''
+            
+            sphereVelocity = sphereVelocities[sphereList.index(s)-1]
 
-            e = u.item()
+            
+            for u in np.nditer(otherSphereArray, flags=['refs_ok','zerosize_ok']):
 
-            if abs(e.pos.x-s.pos.x) < (e.radius + s.radius) and math.sqrt(((e.pos.x-s.pos.x)**2)+((e.pos.y-s.pos.y)**2)+(((e.pos.z-s.pos.z)**2))) < s.radius*2:
-                if sphereVelocity.x > 0:
-                    sphereVelocity.x = -sphereVelocity.x
-                else:
-                    sphereVelocity = abs(sphereVelocity.x)
-                if debugMode == True:
+                e = u.item()
+
+                if abs(e.pos.x-s.pos.x) < (e.radius + s.radius) and math.sqrt(((e.pos.x-s.pos.x)**2)+((e.pos.y-s.pos.y)**2)+(((e.pos.z-s.pos.z)**2))) < s.radius*2:
+                    if sphereVelocity.x > 0:
+                        sphereVelocity.x = -sphereVelocity.x
+                    else:
+                        sphereVelocity.x = abs(sphereVelocity.x)
+                    if debugMode is True:
+                        print(f'X Collision at: {s.pos.x}')
+
+                elif abs(e.pos.y-s.pos.y) < (e.radius + s.radius) and math.sqrt(((e.pos.x-s.pos.x)**2)+((e.pos.y-s.pos.y)**2)+(((e.pos.z-s.pos.z)**2))) < s.radius*2:
+                    if sphereVelocity.y > 0:
+                        sphereVelocity.y = -sphereVelocity.y
+                    else:
+                        sphereVelocity.y = abs(sphereVelocity.y)
+                    if debugMode is True:
+                        print(f'X Collision at: {s.pos.y}')
+                    
+                elif abs(e.pos.z-s.pos.z) < (e.radius + s.radius) and math.sqrt(((e.pos.x-s.pos.x)**2)+((e.pos.y-s.pos.y)**2)+(((e.pos.z-s.pos.z)**2))) < s.radius*2:
+                    if sphereVelocity.z > 0:
+                        sphereVelocity.z = -sphereVelocity.z
+                    else:
+                        sphereVelocity.z = abs(sphereVelocity.z)
+                    if debugMode is True:
+                        print(f'X Collision at: {s.pos.z}')
+
+            if s.pos.x + s.radius >= boundingField[0]/2 or s.pos.x - s.radius <= -boundingField[0]/2:
+                sphereVelocity.x = -sphereVelocity.x
+                if debugMode is True:
                     print(f'X Collision at: {s.pos.x}')
 
-            elif abs(e.pos.y-s.pos.y) < (e.radius + s.radius) and math.sqrt(((e.pos.x-s.pos.x)**2)+((e.pos.y-s.pos.y)**2)+(((e.pos.z-s.pos.z)**2))) < s.radius*2:
-                if sphereVelocity.y > 0:
-                    sphereVelocity.y = -sphereVelocity.y
-                else:
-                    sphereVelocity = abs(sphereVelocity.y)
-                if debugMode == True:
-                    print(f'X Collision at: {s.pos.y}')
+            if s.pos.y + s.radius >= boundingField[1]/2 or s.pos.y - s.radius <= -boundingField[1]/2:
+                sphereVelocity.y = -sphereVelocity.y
+                if debugMode is True:
+                    print(f'Y Collision at: {s.pos.y}')
                 
-            elif abs(e.pos.z-s.pos.z) < (e.radius + s.radius) and math.sqrt(((e.pos.x-s.pos.x)**2)+((e.pos.y-s.pos.y)**2)+(((e.pos.z-s.pos.z)**2))) < s.radius*2:
-                if sphereVelocity.z > 0:
-                    sphereVelocity.z = -sphereVelocity.z
-                else:
-                    sphereVelocity = abs(sphereVelocity.z)
-                if debugMode == True:
-                    print(f'X Collision at: {s.pos.z}')
-                    
-        sphereVelocity = sphereVelocities[sphereList.index(s)]
+            if s.pos.z + s.radius >= boundingField[2]/2 or s.pos.z - s.radius <= -boundingField[2]/2:
+                sphereVelocity.z = -sphereVelocity.z
+                if debugMode is True:
+                    print(f'Z Collision at: {s.pos.z}')
 
-        if s.pos.x + s.radius >= boundingField[0]/2 or s.pos.x - s.radius <= -boundingField[0]/2:
-            sphereVelocity.x = -sphereVelocity.x
-            if debugMode == True:
-                print(f'X Collision at: {s.pos.x}')
+            try:
+                s.pos.x += (sphereVelocity.x * dt)
+            except AttributeError:
+                print('X Attribute Error!')
+                print(s.pos, s.color, sphereVelocity, t)
+                s.color = vp.vector(255,0,255)
+                scene.pause()
+                scene.waitfor('keydown') 
 
-        if s.pos.y + s.radius >= boundingField[1]/2 or s.pos.y - s.radius <= -boundingField[1]/2:
-            sphereVelocity.y = -sphereVelocity.y
-            if debugMode == True:
-                print(f'Y Collision at: {s.pos.y}')
+            try:
+                s.pos.y += (sphereVelocity.y * dt)
+            except AttributeError:
+                print('Y Attribute Error!')
+                print(s.pos, s.color, sphereVelocity, t)
+                s.color = vp.vector(255,0,255)
+                scene.pause()
+                scene.waitfor('keydown') 
+                
+            try:
+                s.pos.z += (sphereVelocity.z * dt)
+            except AttributeError:
+                print('Z Attribute Error!')
+                print(s.pos, s.color, sphereVelocity, t)
+                s.color = vp.vector(255,0,255)
+                scene.pause()
+                scene.waitfor('keydown')
+                
+
+            sphereVelocities[sphereList.index(s)-1].y += -9.81* dt
+
+            if sphereVelocities[sphereList.index(s)-1].y < 0:
+                s.color = vp.vector(1, 0, 0)
+            elif sphereVelocities[sphereList.index(s)-1].y > 0: 
+                s.color = vp.vector(0, 0 , 1)
+            elif sphereVelocities[sphereList.index(s)-1].y == 0:
+                s.color = vp.vector(0, 1 , 0)
             
-        if s.pos.z + s.radius >= boundingField[2]/2 or s.pos.z - s.radius <= -boundingField[2]/2:
-            sphereVelocity.z = -sphereVelocity.z
-            if debugMode == True:
-                print(f'Z Collision at: {s.pos.z}')
 
-        s.pos = s.pos + (sphereVelocity * dt) + (1/2 * gravityVector * dt**2)
-
-        sphereVelocity =  vp.vector(sphereVelocity.x,sphereVelocity.y,sphereVelocity.z)
-
+    except RuntimeError:
+        
